a6ee624bd9f669e7450151cbd47e2619742edeeb
Aroyan <abssneakers@gmail.com>
2021-09-24 23:46:23 +0700

    Initial commit, my first react app

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Hero extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Main Section</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Navbar.css';
+import Logo from './logo.svg';
+import Button from '../Button';
+import Hamburger from './Hamburger.svg';
+
+export default function Navbar() {
+  return (
+    <div className="nav" id="myNav">
+      <a href="#home">
+        <img src={Logo} alt="Logo" />
+      </a>
+
+      {/*This is Comment*/}
+
+      <div className="navlist">
+        <a href="#pricing">Pricing</a>
+        <a href="#product">Product</a>
+        <a href="#about">About Us</a>
+        <a href="#carrers">Carrers</a>
+        <a href="#community">Community</a>
+        <a href="javascript:void(0);" className="icon" onClick="myFunction()">
+          <img src={Hamburger} alt="Hamburger menu" />
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
+
+      <Button />
+    </div>
+  );
+}
